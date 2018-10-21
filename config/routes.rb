90ea575b06a519a007a1c821957487c6e5522e1d
Phani-kanth <phanikanth@intellolabs.com>
2018-10-21 17:27:17 +0530

    Adding devise controllers for custom modifications to the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :home
-  root to: "home#index"
+    
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    devise_for :users, controllers: { sessions: 'users/sessions'}
+
+    resources :home
+
+    root to: "home#index"
+
 end
